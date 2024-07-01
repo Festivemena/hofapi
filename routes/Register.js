f9a8c9eff8d5d6607fa13e20798b3e908b0b54b9
Efemena <88979259+Festivemena@users.noreply.github.com>
2024-07-01 07:34:32 +0100

    Update Register.js

--- routes/Register.js
+++ routes/Register.js
@@ -5,7 +5,16 @@ const Register = require('../models/Register');
 const router = express.Router();
 
 // Register route
-router.post('/', async (req, res) => {
+router.route('/')
+  .get(async (req, res) => {
+    try {
+      const registrations = await Register.find();
+      res.status(200).json(registrations);
+    } catch (error) {
+      res.status(500).json({ message: 'Server error' });
+    }
+  })
+  .post('/', async (req, res) => {
   try {
     const { name, email, businessName, phoneNumber, proposal } = req.body;
     const newRegister = new Register({ name, email, businessName, phoneNumber, proposal });
