eb080982e38974526e6c793e432581a853013260
Efemena <88979259+Festivemena@users.noreply.github.com>
2024-11-30 15:44:03 +0100

    Update Register.js

--- models/Register.js
+++ models/Register.js
@@ -1,12 +1,16 @@
-// models/Register.js
 const mongoose = require('mongoose');
 
 const RegisterSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  businessName: { type: String, required: true },
+  businessName: { type: String, required: false }, // Made optional as not all categories may require it
   phoneNumber: { type: String, required: true },
-  proposal: { type: String, required: true },
-});
+  proposal: { type: String, required: false }, // Made optional as not all categories may require it
+  category: { 
+    type: String, 
+    required: true, 
+    enum: ['Business Buildup', 'Ministry Buildup', 'Steward\'s Buildup', 'Believer\'s Buildup'] 
+  },
+}, { timestamps: true }); // Automatically adds createdAt and updatedAt fields
 
 module.exports = mongoose.model('Register', RegisterSchema);
